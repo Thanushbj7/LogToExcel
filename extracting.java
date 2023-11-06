2b159bda9bf8ebbb068161da78ded3474660e7fb
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2023-11-06 11:11:52 +0530

    Update extracting.java

--- extracting.java
+++ extracting.java
@@ -170,3 +170,157 @@ public class AutomatedUnzipAndReadLogFiles {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class CombinedUnzipAndLogExcelUpdater {
+
+    // Define the log file path
+    static String logFilePath = "";
+    // Define the Excel file path
+    static String excelFilePath = "extract-excel.xlsx";
+
+    // Regular expression for extracting date and time
+    static String datetimePattern = "(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})";
+
+    // Regular expression for extracting a number before the word 'successful'
+    static String numberPattern = "(\\d+)\\s+successful";
+
+    // Function to extract data from the log file based on TABLE_NAME column in Excel
+    static List<String> extractDataFromLog(String logFilePath, String excelFilePath) {
+        List<String> extractedData = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(logFilePath));
+            String line;
+            String firstRecord = null;
+            String lastRecord = null;
+            String decryptingRecord = null;
+            String lastSuccessful = null;
+            while ((line = reader.readLine()) != null) {
+                Pattern pattern = Pattern.compile(datetimePattern);
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    if (firstRecord == null) {
+                        firstRecord = matcher.group(0);
+                    }
+                    lastRecord = matcher.group(0);
+                }
+                if (line.contains("Decrypting...")) {
+                    line = reader.readLine(); // read the next line
+                    matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        decryptingRecord = matcher.group(0);
+                    }
+                }
+                pattern = Pattern.compile(numberPattern);
+                matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    lastSuccessful = matcher.group(1);
+                }
+            }
+            reader.close();
+            extractedData.add(firstRecord);
+            extractedData.add(lastRecord);
+            extractedData.add(decryptingRecord);
+            extractedData.add(lastSuccessful);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return extractedData;
+    }
+
+    // Function to update Excel file with extracted data
+    static void updateExcelWithData(int rowNumber, String excelFilePath, List<String> extractedData) {
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+            Row row = sheet.getRow(rowNumber);
+            if (row == null) {
+                row = sheet.createRow(rowNumber);
+            }
+            Cell cell1 = row.createCell(0);
+            cell1.setCellValue(extractedData.get(0));
+            Cell cell2 = row.createCell(6);
+            cell2.setCellValue(extractedData.get(1));
+            Cell cell3 = row.createCell(5);
+            cell3.setCellValue(extractedData.get(2));
+            Cell cell4 = row.createCell(4);
+            cell4.setCellValue(extractedData.get(3));
+            inputStream.close();
+            FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+            System.out.println("Data successfully updated in Excel file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Directory containing zip files
+        String zipDirectory = "path/to/your/zip/files";
+
+        try {
+            File dir = new File(zipDirectory);
+            File[] zipFiles = dir.listFiles((dir1, name) -> name.endsWith(".zip"));
+
+            if (zipFiles != null) {
+                for (File zipFile : zipFiles) {
+                    System.out.println("Unzipping: " + zipFile.getName());
+
+                    try (FileInputStream fis = new FileInputStream(zipFile);
+                         ZipInputStream zis = new ZipInputStream(fis)) {
+
+                        ZipEntry entry;
+                        while ((entry = zis.getNextEntry()) != null) {
+                            if (entry.getName().endsWith(".log")) {
+                                System.out.println("Reading log file: " + entry.getName());
+
+                                // Read the log file content here
+                                // You can use a BufferedReader to read the content line by line
+                                BufferedReader reader = new BufferedReader(new InputStreamReader(zis));
+                                String line;
+                                while ((line = reader.readLine()) != null) {
+                                    // Process each line of the log file
+                                    System.out.println(line);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                System.out.println("No zip files found in the directory.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
