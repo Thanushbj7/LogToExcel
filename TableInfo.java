da96fbd9334728e4b29416d1335abe13c211e2eb
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2023-11-17 19:49:54 +0530

    Update TableInfo.java

--- TableInfo.java
+++ TableInfo.java
@@ -148,3 +148,106 @@ PS Microsoft.PowerShell.Core\FileSystem::\\pstrwdfs9031\Profiles\G-FR1313-NonPri
         at LogFileExtract.processLogFiles(LogFileExtract.java:64)
         at LogFileExtract.main(LogFileExtract.java:25)
 PS Microsoft.PowerShell.Core\FileSystem::\\pstrwdfs9031\Profiles\G-FR1313-NonPrivileged\i73
+
+
+	import java.io.*;
+import java.util.zip.*;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class LogFileExtract {
+    public static void main(String[] args) {
+        // Provide the path to the zip file
+        String zipFilePath = "H:/ZipFile/Copy_Tables_extract_log_files_20231026.20001698364809.zip";
+
+        // Provide the path where you want to extract the zip file
+        String extractFolderPath = "H:/ZipFile";
+
+        // Provide the path where you want to save the Excel file
+        String excelFilePath = "C:/Users/i733581/Workspace/file.xlsx";
+
+        try {
+            // Unzip the specified file
+            unzipFile(zipFilePath, extractFolderPath);
+
+            // Create an Excel file and write headers
+            createExcelFile(excelFilePath);
+
+            // Process log files
+            processLogFiles(extractFolderPath, excelFilePath);
+
+            System.out.println("Processing completed successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void unzipFile(String zipFilePath, String extractFolderPath) throws IOException {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                String filePath = extractFolderPath + File.separator + zipEntry.getName();
+                try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+                    byte[] bytesIn = new byte[1024];
+                    int read;
+                    while ((read = zis.read(bytesIn)) != -1) {
+                        bos.write(bytesIn, 0, read);
+                    }
+                }
+                zipEntry = zis.getNextEntry();
+            }
+        }
+    }
+
+    private static void createExcelFile(String excelFilePath) throws IOException {
+        try (FileWriter writer = new FileWriter(excelFilePath)) {
+
+            writer.write("RUN_DATE,RUN_CYCLE,TABLE_NAME,WEEKLY_DAILY,RECORD_COUNT,START_DATE,END_DATE\n");
+        }
+    }
+
+    private static void processLogFiles(String extractFolderPath, String excelFilePath) throws IOException {
+        File folder = new File(extractFolderPath);
+        File[] listOfFiles = folder.listFiles();
+
+        if (listOfFiles != null) {
+            for (File file : listOfFiles) {
+                if (file.isFile() && file.getName().endsWith(".log")) {
+                    processLogFile(file, excelFilePath);
+                }
+            }
+        }
+    }
+
+    private static void processLogFile(File logFile, String excelFilePath) throws IOException {
+        try (Scanner scanner = new Scanner(logFile);
+                FileWriter writer = new FileWriter(excelFilePath, true)) {
+            // Extract details from the log file
+            String tableName = logFile.getName().contains("sfdcRegistrationMapExtractProcess")
+                    ? "SFDC_W_TR_REGISTRATION_MAP"
+                    : "";
+            String runDate = scanner.nextLine(); // Assuming the first line contains date and time
+            String startDate = "";
+            String endDate = "";
+            int recordCount = 0;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains("Decrypting...")) {
+                    // Extract date and time below the "decrypting..." line
+                    startDate = line.substring(0, line.indexOf("decrypting...")).trim();
+                }
+                // Extract date and time from the last line
+                endDate = line;
+                if (line.contains("successful")) {
+                    // Extract the number before the "successful" string
+                    recordCount = line.contains("successful") ? Integer.parseInt(line.substring(0, line.indexOf("successful")).trim());
+                }
+            }
+
+            // Write the extracted details to the Excel file
+            writer.write(runDate + "," + "" + "," + tableName + "," + "" + "," + recordCount + "," + startDate + ","
+                    + endDate + "\n");
+        }
+    }
+}
